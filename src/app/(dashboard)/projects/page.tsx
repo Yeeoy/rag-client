e4fa37703a16e4a6b38c42e35a860803375ecda9
十弥 <shimi@shimideMacBook-Pro.local>
2025-12-28 11:59:25 +0800

    feat: user authentication system

---
+++ src/app/(dashboard)/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProjectsPage() {
+    return (
+        <div>
+            This page shows the list of all the projects associated with the
+            logged user
+        </div>
+    );
+}
+
+export default ProjectsPage;
